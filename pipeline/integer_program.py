efce94154058b48b5760454ad7d870b4b6776058
pstarszyk <pstarszyk@gmail.com>
2022-11-14 14:50:23 -0500

    remodel MIP as CP_SAT

--- pipeline/integer_program.py
+++ pipeline/integer_program.py
@@ -1,49 +1,50 @@
-import cvxpy as cp
 import numpy as np
-
+from ortools.sat.python import cp_model
 from config.core import config
 from typing import List
 
 
 def solve_board(*, board: List[List[int]]) -> List[List[int]]:
-    board = np.array(board)
-    X = dict()
-    for k in range(9):
-        X['X%s' % (k + 1)] = cp.Variable((9, 9), boolean=True)
+    model = cp_model.CpModel()
+    X = np.array([[[None for _ in range(9)] for _ in range(9)] for _ in range(9)])
 
-    constraints = list()
-    exp = 0
-    for k in range(9):
-        exp += X['X%s' % (k + 1)]
-    constraints.append(exp >= 1)
-    constraints.append(exp <= 1)
+    # Variables
+    for row in range(9):
+        for col in range(9):
+            for num in range(9):
+                if num + 1 == board[row][col]:
+                    X[row, col, num] = model.NewIntVar(1, 1, 'x_%s%s%s' % (row + 1, col + 1, num + 1))
+                elif num + 1 != board[row][col] and board[row][col] != 0:
+                    X[row, col, num] = model.NewIntVar(0, 0, 'x_%s%s%s' % (row + 1, col + 1, num + 1))
+                else:
+                    X[row, col, num] = model.NewIntVar(0, 1, 'x_%s%s%s' % (row + 1, col + 1, num + 1))
 
-    for k in range(9):
-        lower_bound = np.zeros((9, 9))
-        lower_bound[np.where(board == k + 1)] = 1
-        constraints.append(lower_bound <= X['X%s' % (k + 1)])
-        for i in range(9):
-            constraints.append(cp.sum(X['X%s' % (k + 1)][i, :]) >= 1)
-            constraints.append(cp.sum(X['X%s' % (k + 1)][i, :]) <= 1)
-        for j in range(9):
-            constraints.append(cp.sum(X['X%s' % (k + 1)][:, j]) >= 1)
-            constraints.append(cp.sum(X['X%s' % (k + 1)][:, j]) <= 1)
-        for i in range(3):
-            for j in range(3):
-                constraints.append(cp.sum(X['X%s' % (k + 1)][i * 3:(i + 1) * 3, j * 3:(j + 1) * 3]) >= 1)
-                constraints.append(cp.sum(X['X%s' % (k + 1)][i * 3:(i + 1) * 3, j * 3:(j + 1) * 3]) <= 1)
+    # Constraints
+    for i in range(9):
+        for row in range(9):
+            model.Add(sum(X[row, :, i]) == 1)
+        for col in range(9):
+            model.Add(sum(X[:, col, i]) == 1)
+        for col in range(9):
+            model.Add(sum(X[i, col, :]) == 1)
+        for h in range(3):
+            for v in range(3):
+                model.Add(sum(sum(X[h * 3:(h + 1) * 3, v * 3:(v + 1) * 3, i])) == 1)
 
     try:
-        objective = cp.Minimize(1)
-        prob = cp.Problem(objective, constraints)
-        prob.solve(solver=config.pipeline_config.solver_mappings['solver'])
-        sol = np.zeros((9, 9), dtype=int)
-        for k in range(9):
-            sol += np.rint(np.multiply(X['X%s' % (k + 1)].value, k + 1)).astype(int)
-        sol = sol.tolist()
+        solver = cp_model.CpSolver()
+        status = solver.Solve(model)
+
+        sol = [[None for _ in range(9)] for _ in range(9)]
+        for row in range(9):
+            for col in range(9):
+                for num in range(9):
+                    if solver.Value(X[row, col, num]) == 1:
+                        sol[row][col] = num + 1
+                        break
 
     except Exception as e:
         print(e)
-        sol = board.tolist()
+        sol = board
 
     return sol
